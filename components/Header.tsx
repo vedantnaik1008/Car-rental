0922ec6ee9c5fdb6a1f91546794a2c1c5692d645
tallman <vedunaik@gmail.com>
2024-01-07 21:06:53 +0530

    +styles dynamically using pathname

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import Nav from '@/components/Nav';
 import useScroll from '../hooks/useScroll';
 import Hamburger from './MobileNav';
+import { usePathname } from 'next/navigation';
 
 interface HeaderProps {
   whiteBackground: boolean;
@@ -11,12 +12,14 @@ interface HeaderProps {
 
 const Header = ({ whiteBackground }: HeaderProps) => {
   const isScrolled = useScroll(whiteBackground);
-
+  const pathname = usePathname();
+  const dynamicStyle = pathname === '/faqs' || pathname === '/about-us' || pathname === '/contact'
+      ? 'path-show'
+      : '';
   return (
     <header
-      className={`fixed z-50 w-full py-4 drop-shadow-md sm:px-[10px] md:px-[20px] ${
-        isScrolled ? 'bg-white' : ''
-      } ${whiteBackground ? 'bg-white' : ''}`}>
+      className={`${dynamicStyle} fixed z-50 w-full py-4 drop-shadow-md sm:px-[10px] md:px-[20px] ${
+        isScrolled ? 'bg-white' : ''}`}>
       <div className='mx-auto flex max-w-7xl items-center justify-center'>
         <Nav />
       </div>

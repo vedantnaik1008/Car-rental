6b9261d2c491d675ad229843c656fe6a42f5d168
tallman <vedunaik@gmail.com>
2024-01-19 17:10:51 +0530

    removed featured car setting

--- lib/SliderSetting.tsx
+++ lib/SliderSetting.tsx
@@ -12,43 +12,4 @@ export const TestimonialSettings = {
   prevArrow: <IoIosArrowBack color='rgb(203, 213, 225)' />,
 };
 
-export const FeaturedCarSettings = {
-  dots: true,
-  infinite: true,
-  autoplay: false,
-  slidesToShow: 3,
-  slidesToScroll: 3,
-  arrows: true,
-  nextArrow: <IoIosArrowForward color='rgb(203, 213, 225)' />,
-  prevArrow: <IoIosArrowBack color='rgb(203, 213, 225)' />,
-  responsive: [
-    {
-      breakpoint: 1440,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true,
-      },
-    },
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        infinite: true,
-        dots: true,
-      },
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: false,
-      },
-    },
-  ],
-};
-
 export { Slider };

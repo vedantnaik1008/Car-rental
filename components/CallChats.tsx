d3248c030e1291b35e6b5299d07df3bc047416c4
tallman <vedunaik@gmail.com>
2024-02-26 16:27:43 +0530

    +whatsapp&phone links

--- components/CallChats.tsx
+++ components/CallChats.tsx
@@ -6,13 +6,13 @@ const CallChats = () => {
     <>
       <Link
         aria-label='Call to rent car'
-        href={'/'}
+        href={'tel:+917620148036'}
         className='rounded-xl bg-sky-900/90 px-9 py-2.5 text-white transition-shadow duration-200 hover:bg-sky-900/85 hover:shadow-lg hover:shadow-sky-900/10 focus:outline-none focus-visible:ring focus-visible:ring-sky-700'>
         <IoMdCall color='white' size='21px' />
       </Link>
       <Link
         aria-label='Whatsapp to rent car'
-        href={'/'}
+        href={`https://wa.me/917620148036?text=Hi,%0aI want to book *Rent car in Goa* on OwnWheelz.com`}
         className='rounded-xl bg-emerald-500 px-9 py-2.5 text-white transition-shadow duration-200 hover:bg-emerald-500/90 hover:shadow-lg hover:shadow-emerald-500/10 focus:outline-none focus-visible:ring focus-visible:ring-emerald-600'>
         <IoLogoWhatsapp color='white' size='21px' />
       </Link>
--- components/FeaturedCars.tsx
+++ components/FeaturedCars.tsx
@@ -16,61 +16,61 @@ const FeaturedCars = () => {
               Featured Cars
             </h2>
           </div>
-          <div className='flex flex-wrap gap-10 justify-center items-center'>
-              {cars
-                .map((res: Cars) => (
-                  <div key={res.id} className='w-[300px] rounded-[30px] py-5 outline-none'>
-                    <Image
-                      src={res.image}
-                      alt='items-image'
-                      sizes='200'
-                      width={200}
-                      height={200}
-                      className=' mx-auto mb-4 h-[250px] w-full rounded-lg object-contain object-center'
-                    />
-                    <div className='mb-5 font-semibold tracking-wide text-sky-950'>
-                      <p className='mb-2 text-sky-950'>
-                        {res.brandName} {res.model}
-                      </p>
-                      <p className='text-sky-500'>
-                        {res.price}
-                        <span className='font-normal text-gray-600'>/day</span>
-                      </p>
-                    </div>
-                    <div className='grid grid-cols-2 items-center gap-x-12 gap-y-4 text-gray-600'>
-                      <p className='flex items-center gap-x-2 text-[14px]'>
-                        <GoPeople size='20px' color='gray' />
-                        {res.features.seats}
-                      </p>
-                      <p className='flex items-center gap-x-2 pl-[2px] text-[14px]'>
-                        <BsFuelPumpFill size='20px' color='gray' />
-                        {res.features.fuel}
-                      </p>
-                      <p className='flex items-center gap-x-2 text-[14px]'>
-                        <ImMeter size='20px' color='gray' />
-                        {res.features.mileage}
-                      </p>
-                      <p className='flex items-center gap-x-2 text-[14px]'>
-                        <PiSteeringWheelBold size='20px' color='gray' className='shrink-0' />
-                        <span className='truncate'>{res.features.gear}</span>
-                      </p>
-                    </div>
-                    <hr className='my-5 border border-slate-200' />
-                    <div className='flex flex-col items-center justify-between gap-5'>
-                      <Link
-                        href={'/'}
-                        className='w-full rounded-xl bg-sky-950/90 px-8 py-2.5 text-center text-sm font-semibold text-white transition-shadow duration-200 hover:bg-sky-950/85 focus:outline-none focus-visible:ring focus-visible:ring-sky-700'>
-                        Call to Book
-                      </Link>
-                      <Link
-                        href={'/'}
-                        className='w-full rounded-xl bg-emerald-500 px-8 py-2.5 text-center text-sm font-semibold text-white transition-shadow duration-200 hover:bg-emerald-500/90  focus:outline-none focus-visible:ring focus-visible:ring-emerald-600'>
-                        Book on WhatsApp
-                      </Link>
-                    </div>
+          <div className='flex flex-wrap items-center justify-center gap-10'>
+            {cars
+              .map((res: Cars) => (
+                <div key={res.id} className='w-[300px] rounded-[30px] py-5 outline-none'>
+                  <Image
+                    src={res.image}
+                    alt='items-image'
+                    sizes='200'
+                    width={200}
+                    height={200}
+                    className=' mx-auto mb-4 h-[250px] w-full rounded-lg object-contain object-center'
+                  />
+                  <div className='mb-5 font-semibold tracking-wide text-sky-950'>
+                    <p className='mb-2 text-sky-950'>
+                      {res.brandName} {res.model}
+                    </p>
+                    <p className='text-sky-500'>
+                      {res.price}
+                      <span className='font-normal text-gray-600'>/day</span>
+                    </p>
                   </div>
-                ))
-                .splice(4, 4)}
+                  <div className='grid grid-cols-2 items-center gap-x-12 gap-y-4 text-gray-600'>
+                    <p className='flex items-center gap-x-2 text-[14px]'>
+                      <GoPeople size='20px' color='gray' />
+                      {res.features.seats}
+                    </p>
+                    <p className='flex items-center gap-x-2 pl-[2px] text-[14px]'>
+                      <BsFuelPumpFill size='20px' color='gray' />
+                      {res.features.fuel}
+                    </p>
+                    <p className='flex items-center gap-x-2 text-[14px]'>
+                      <ImMeter size='20px' color='gray' />
+                      {res.features.mileage}
+                    </p>
+                    <p className='flex items-center gap-x-2 text-[14px]'>
+                      <PiSteeringWheelBold size='20px' color='gray' className='shrink-0' />
+                      <span className='truncate'>{res.features.gear}</span>
+                    </p>
+                  </div>
+                  <hr className='my-5 border border-slate-200' />
+                  <div className='flex flex-col items-center justify-between gap-5'>
+                    <Link
+                      href={'tel:+917620148036'}
+                      className='w-full rounded-xl bg-sky-950/90 px-8 py-2.5 text-center text-sm font-semibold text-white transition-shadow duration-200 hover:bg-sky-950/85 focus:outline-none focus-visible:ring focus-visible:ring-sky-700'>
+                      Call to Book
+                    </Link>
+                    <Link
+                      href={`https://wa.me/917620148036?text=Hi,%0aI want to book *Rent ${res.brandName} ${res.model} (${res.features.gear}) in Goa* on OwnWheelz.com`}
+                      className='w-full rounded-xl bg-emerald-500 px-8 py-2.5 text-center text-sm font-semibold text-white transition-shadow duration-200 hover:bg-emerald-500/90  focus:outline-none focus-visible:ring focus-visible:ring-emerald-600'>
+                      Book on WhatsApp
+                    </Link>
+                  </div>
+                </div>
+              ))
+              .splice(4, 4)}
           </div>
           <div className='mt-10 flex justify-center'>
             {/* <Link

2b11fa456583ee9ff9f3fa5f9303bfecde15c4b4
Shaditya Kinlekar <shadityakinlekar20@gmail.com>
2024-02-27 16:11:55 +0530

    changed logo text container to <p> and added sm600

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,33 +1,34 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      animation: { shimmer: 'shimmer 2s linear infinite' },
-      keyframes: {
-        shimmer: { from: { backgroundPosition: '0 0' }, to: { backgroundPosition: '-200% 0' } },
-      },
-      screens: {
-        sm: '320px',
-        sm360: '360px',
-        lg964: '964px',
-        lgg: '826px',
-      },
-      fontFamily: {
-        zen: ['var(--font-zenDots)'],
-        pop_inter: ['var(--font-inter)', 'var(--font-poppins)', 'sans-serif'],
-      },
-      backgroundImage: {
-        hero: "url('/assets/hero-car.webp')",
-      },
-    },
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			animation: { shimmer: 'shimmer 2s linear infinite' },
+			keyframes: {
+				shimmer: { from: { backgroundPosition: '0 0' }, to: { backgroundPosition: '-200% 0' } },
+			},
+			screens: {
+				sm: '320px',
+				sm360: '360px',
+				sm600: '600px',
+				lg964: '964px',
+				lgg: '826px',
+			},
+			fontFamily: {
+				zen: ['var(--font-zenDots)'],
+				pop_inter: ['var(--font-inter)', 'var(--font-poppins)', 'sans-serif'],
+			},
+			backgroundImage: {
+				hero: "url('/assets/hero-car.webp')",
+			},
+		},
 
-    plugins: [],
-  },
+		plugins: [],
+	},
 };
 export default config;

b34dccfc1b57eea5ba0c28fb6b04adb507286748
Shaditya Kinlekar <shadityakinlekar20@gmail.com>
2024-02-28 17:43:55 +0530

    added favicon

--- components/FixedNavButtons.tsx
+++ components/FixedNavButtons.tsx
@@ -15,7 +15,7 @@ export default function Fixed_Nav_Buttons() {
 			<a
 				aria-label='Call to contact OwnWheelz.com'
 				href={'tel:+91'}
-				className='gap flex items-center justify-center rounded border-4 border-gray-400 bg-white py-2.5  font-medium text-black transition-shadow duration-200 hover:bg-gray-100 hover:shadow-lg hover:shadow-sky-900/10 focus:outline-none focus-visible:ring focus-visible:ring-gray-400 max-md:w-1/2 md:rounded-md md:border-[2.5px] md:border-black/80 md:bg-opacity-0 md:px-4 md:py-2'>
+				className='gap flex items-center justify-center rounded border-4 border-gray-400 bg-white py-2 font-medium text-black transition-shadow duration-200 hover:bg-gray-100 hover:shadow-lg hover:shadow-sky-900/10 focus:outline-none focus-visible:ring focus-visible:ring-gray-400 max-md:w-1/2 md:rounded-md md:border-[2.5px] md:border-black/80 md:bg-opacity-0 md:px-4'>
 				<IoMdCall color='' size='21px' />
 				<span>Talk to Us</span>
 			</a>
@@ -24,7 +24,7 @@ export default function Fixed_Nav_Buttons() {
 				aria-label='Whatsapp to contact OwnWheelz.com'
 				target='_blank'
 				href={`https://wa.me/917620062784?text=Hi,%0acan I know about your *services* on OwnWheelz.com`}
-				className='flex items-center justify-center gap-2 rounded border-4 border-emerald-900 bg-emerald-500 py-2.5 font-medium text-white transition-shadow duration-200 hover:bg-emerald-500/90 hover:shadow-lg hover:shadow-emerald-500/10 focus:outline-none focus-visible:ring focus-visible:ring-emerald-900 max-md:w-1/2 md:rounded-md md:border-none md:px-4 md:py-2'>
+				className='flex items-center justify-center gap-2 rounded border-4 border-emerald-900 bg-emerald-500 py-2 font-medium text-white transition-shadow duration-200 hover:bg-emerald-500/90 hover:shadow-lg hover:shadow-emerald-500/10 focus:outline-none focus-visible:ring focus-visible:ring-emerald-900 max-md:w-1/2 md:rounded-md md:border-none md:px-4'>
 				<IoLogoWhatsapp color='white' size='21px' />
 				<span>Chat Now</span>
 			</Link>

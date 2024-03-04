d94ef980c17851abc8ff0390fb1a5ee8ef8e598c
tallman <vedunaik@gmail.com>
2024-03-04 11:26:06 +0530

    added infinite

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Hero from '@/components/Hero';
 import RentalProcess from '@/components/RentalProcess';
 import OurServices from '@/components//OurServices';
 import FeaturedCars from '@/components/FeaturedCars';
-import Testimonials from '@/components/Testimonials';
+import {Testimonials} from '@/components/Testimonials';
 import Faqs from '@/components/Faqs';
 
 export default function Home() {

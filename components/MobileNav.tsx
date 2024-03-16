2341afc8b0fa5b99ec94af1e73420df894f6d8e0
tallman <vedunaik@gmail.com>
2024-03-16 15:12:14 +0530

    fall  down menu

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,100 +1,107 @@
 'use client';
-import { useState } from 'react';
 import Link from 'next/link';
 import Logo from './logo';
 import { FaBarsStaggered, IoClose, IoIosArrowDown } from '@/lib/ReactIcons';
 import { Menu } from '@headlessui/react';
 import CallChats from './CallChats';
 
-const MobileNav = () => {
-  const [open, isOpen] = useState(false);
+type Props = {
+	open: boolean;
+	setOpen: (open: boolean) => void;
+};
 
-  return (
-		<div id='navbar' className='w-full'>
+const MobileNav = ({ open, setOpen }: Props) => {
+	return (
+		<div className='w-full'>
 			<div className={`w-full lg:hidden`}>
-				<div className='flex items-center justify-between relative'>
+				<div className='relative flex items-center justify-between'>
 					<Logo textColor='text-sky-950' spanColor='text-sky-700' />
-					<CallChats className='hidden md:flex md:items-center md:gap-4 md:absolute right-10 lg:hidden' />
-					<div onClick={() => isOpen(true)} className='cursor-pointer'>
-						<FaBarsStaggered size='25px' />
-					</div>
-					<div
-						className={
-							open
-								? 'absolute -right-10 -top-4 z-[50] h-screen w-[80%] translate-x-0 rounded-l-md bg-white px-5 py-10 shadow-2xl transition-all duration-300 ease-in-out sm:block lg:hidden'
-								: 'absolute -right-10 -top-4 z-[50] h-screen w-[80%] translate-x-[200%] rounded-l-md bg-white px-5 py-10  shadow-2xl transition-all duration-300 ease-in-out sm:block lg:hidden'
-						}>
-						<div onClick={() => isOpen(false)} className='absolute left-5 top-5 cursor-pointer'>
-							<IoClose size='25px' />
+					<CallChats className='right-10 hidden md:absolute md:flex md:items-center md:gap-4 lg:hidden' />
+					{!open ? (
+						<div onClick={() => setOpen(!open)} className='cursor-pointer'>
+							<FaBarsStaggered size='25px' />
 						</div>
-						<ul className='flex h-full flex-col items-center justify-start gap-y-8'>
-							<li className=''>
-								<Link
-									href='/'
-									className='cursor-pointer text-black hover:font-semibold hover:text-gray-600'>
-									Home
-								</Link>
-							</li>
-							<li className='group relative'>
-								<Menu as='div' className='relative inline-block text-left'>
-									{({ open }) => (
-										<>
-											<Menu.Button className='flex cursor-pointer items-center justify-center hover:font-semibold hover:text-gray-600 focus:outline-none focus-visible:ring focus-visible:ring-sky-500'>
-												Services
-												<IoIosArrowDown
-													className={`ml-0.5 text-[16px] transition-transform duration-300  ease-in-out ${
-														open ? 'rotate-180' : 'rotate-0'
-													}`}
-												/>
-											</Menu.Button>
-											<Menu.Items className='absolute left-0 top-6 mt-2 block  w-[150px] rounded-lg bg-white py-5 text-black outline-none'>
-												<div className='flex flex-col items-center'>
-													<div className='flex flex-col items-start gap-3'>
-														<Menu.Item>
-															<Link
-																href='/browse-cars'
-																className={`hover:font-semibold hover:text-gray-600`}>
-																Rent a Car
-															</Link>
-														</Menu.Item>
-														<Menu.Item>
-															<Link href='/' className={`hover:font-semibold hover:text-gray-600`}>
-																Rent a Bike
-															</Link>
-														</Menu.Item>
-														<Menu.Item>
-															<Link href='/' className={`hover:font-semibold hover:text-gray-600`}>
-																Book a Cab
-															</Link>
-														</Menu.Item>
-													</div>
+					) : (
+						<div className={open ? 'lg:hidden' : 'lg:hidden'}>
+							<div onClick={() => setOpen(false)} className=''>
+								<IoClose size='35px' />
+							</div>
+						</div>
+					)}
+				</div>
+				<div
+					className={
+						open
+							? 'absolute  left-0 top-20 z-[50] w-full translate-y-0 skew-y-0 rotate-0 rounded-xl bg-white px-5 py-10 shadow-2xl transition-all duration-700 ease-in-out lg:hidden'
+							: 'absolute  left-0 top-20 z-[50] w-full translate-y-[300%] skew-y-12 rotate-90 rounded-xl bg-white px-5 py-10  shadow-2xl transition-all duration-700 ease-in-out lg:hidden'
+					}>
+					<ul className='flex h-full flex-col items-center justify-start gap-y-8 py-10'>
+						<li className=''>
+							<Link
+								href='/'
+								className='cursor-pointer text-black hover:font-semibold hover:text-gray-600'>
+								Home
+							</Link>
+						</li>
+						<li className='group relative'>
+							<Menu as='div' className='relative inline-block text-left'>
+								{({ open }) => (
+									<>
+										<Menu.Button className='flex cursor-pointer items-center justify-center hover:font-semibold hover:text-gray-600 focus:outline-none focus-visible:ring focus-visible:ring-sky-500'>
+											Services
+											<IoIosArrowDown
+												className={`ml-0.5 text-[16px] transition-transform duration-1000  ease-in-out ${
+													open ? 'rotate-180' : 'rotate-0'
+												}`}
+											/>
+										</Menu.Button>
+										<Menu.Items className='absolute left-0 top-6 mt-2 block  w-[150px] rounded-lg bg-white py-5 text-black outline-none'>
+											<div className='flex flex-col items-center'>
+												<div className='flex flex-col items-start gap-3'>
+													<Menu.Item>
+														<Link
+															href='/browse-cars'
+															className={`hover:font-semibold hover:text-gray-600`}>
+															Rent a Car
+														</Link>
+													</Menu.Item>
+													<Menu.Item>
+														<Link href='/' className={`hover:font-semibold hover:text-gray-600`}>
+															Rent a Bike
+														</Link>
+													</Menu.Item>
+													<Menu.Item>
+														<Link href='/' className={`hover:font-semibold hover:text-gray-600`}>
+															Book a Cab
+														</Link>
+													</Menu.Item>
 												</div>
-											</Menu.Items>
-										</>
-									)}
-								</Menu>
-							</li>
-							<li className=''>
-								<Link
-									href='/about-us'
-									className='cursor-pointer text-black hover:font-semibold hover:text-gray-600'>
-									About us
-								</Link>
-							</li>
-							<li className=''>
-								<Link
-									href='/faqs'
-									className='cursor-pointer text-black hover:font-semibold hover:text-gray-600'>
-									FAQs
-								</Link>
-							</li>
-							<li className=''>
-								<Link href='/contact' className='cursor-pointer text-black hover:text-gray-600'>
-									Contact
-								</Link>
-							</li>
-						</ul>
-					</div>
+											</div>
+										</Menu.Items>
+									</>
+								)}
+							</Menu>
+						</li>
+						<li className=''>
+							<Link
+								href='/about-us'
+								className='cursor-pointer text-black hover:font-semibold hover:text-gray-600'>
+								About us
+							</Link>
+						</li>
+						<li className=''>
+							<Link
+								href='/faqs'
+								className='cursor-pointer text-black hover:font-semibold hover:text-gray-600'>
+								FAQs
+							</Link>
+						</li>
+						<li className=''>
+							<Link href='/contact' className='cursor-pointer text-black hover:text-gray-600'>
+								Contact
+							</Link>
+						</li>
+					</ul>
 				</div>
 			</div>
 		</div>

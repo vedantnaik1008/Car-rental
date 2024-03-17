f11a574904efbd3f3af52ea6587feb91d237c9fe
tallman <vedunaik@gmail.com>
2024-03-17 16:58:40 +0530

    -translate & +block hidden for nav menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,10 @@ import React, { useEffect } from 'react';
 import Nav from '@/components/Nav';
 import Hamburger from './MobileNav';
 import { useState } from 'react';
+import { FaBarsStaggered } from 'react-icons/fa6';
+import { IoClose } from 'react-icons/io5';
+import CallChats from './CallChats';
+import Logo from './logo';
 
 const Header = () => {
 const [open, setOpen] = useState(false);
@@ -25,9 +29,24 @@ const [open, setOpen] = useState(false);
 	return (
 		<header
 			id='navbar'
-			className={`fixed z-20 mx-auto mt-5 w-[95%] py-3.5 drop-shadow-2xl rounded-xl sm:px-[10px] left-[2.5%]`}>
+			className={`fixed z-20 mx-auto mt-5 w-[95%] bg-white py-3.5 drop-shadow-2xl shadow-2xl rounded-xl left-[2.5%] sm:px-[10px]`}>
 			<div className='mx-auto flex max-w-7xl items-center justify-center'>
 				<Nav />
+			</div>
+			<div className='relative flex items-center justify-between lg:hidden'>
+				<Logo textColor='text-sky-950' spanColor='text-sky-700' />
+				<CallChats className='right-10 hidden md:absolute md:flex md:items-center md:gap-4 lg:hidden' />
+				{!open ? (
+					<div onClick={() => setOpen(!open)} className='cursor-pointer'>
+						<FaBarsStaggered size='25px' />
+					</div>
+				) : (
+					<div className={open ? 'lg:hidden' : 'lg:hidden'}>
+						<div onClick={() => setOpen(false)} className=''>
+							<IoClose size='35px' />
+						</div>
+					</div>
+				)}
 			</div>
 			<Hamburger open={open} setOpen={setOpen} />
 		</header>
--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -14,27 +14,7 @@ const MobileNav = ({ open, setOpen }: Props) => {
 	return (
 		<div className='w-full'>
 			<div className={`w-full lg:hidden`}>
-				<div className='relative flex items-center justify-between'>
-					<Logo textColor='text-sky-950' spanColor='text-sky-700' />
-					<CallChats className='right-10 hidden md:absolute md:flex md:items-center md:gap-4 lg:hidden' />
-					{!open ? (
-						<div onClick={() => setOpen(!open)} className='cursor-pointer'>
-							<FaBarsStaggered size='25px' />
-						</div>
-					) : (
-						<div className={open ? 'lg:hidden' : 'lg:hidden'}>
-							<div onClick={() => setOpen(false)} className=''>
-								<IoClose size='35px' />
-							</div>
-						</div>
-					)}
-				</div>
-				<div
-					className={
-						open
-							? 'absolute  left-0 top-20 z-[50] w-full translate-x-0 rounded-xl bg-white px-5 py-10 shadow-2xl transition-all duration-700 ease-in-out lg:hidden'
-							: 'absolute  left-0 top-20 z-[50] w-full translate-x-[300%] rounded-xl bg-white px-5 py-10  shadow-2xl transition-all duration-700 ease-in-out lg:hidden'
-					}>
+				<div className={open ? 'block rounded-b-xl ' : 'hidden rounded-b-xl'}>
 					<ul className='flex h-full flex-col items-center justify-start gap-y-8 py-10'>
 						<li className=''>
 							<Link

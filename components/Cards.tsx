c9c4be25c021a722979234a7c76ec667e89ed77a
tallman <vedunaik@gmail.com>
2024-03-31 15:26:00 +0530

    reduced the length of cards and added extra data in bikes api

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -1,8 +1,10 @@
+'use client'
 import { Cars } from '@/data/API';
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import { BsFuelPumpFill, GoPeople, ImMeter, PiSteeringWheelBold } from '@/lib/ReactIcons';
+import { FaArrowDown } from 'react-icons/fa6';
 
 type Props = {
     res: Cars;
@@ -10,6 +12,7 @@ type Props = {
 }
 
 const Cards = ({res, key}: Props) => {
+	const [open, setOpen] = useState(false)
   return (
 		<>
 			<div key={key} className='rounded-[30px] outline-none'>
@@ -25,12 +28,30 @@ const Cards = ({res, key}: Props) => {
 					<p className='mb-2 text-sky-950'>
 						{res.brandName} {res.model}
 					</p>
-					<p className='text-sky-500'>
-						{res.price}
-						<span className='font-normal text-gray-600'>/day</span>
-					</p>
+					<div className='flex items-center gap-x-2'>
+						<p className='text-sky-500'>
+							{res.price}
+							<span className='font-normal text-gray-600'>/day</span>
+						</p>
+						<button
+							className='flex items-center gap-x-2 rounded-xl border border-black px-2 py-1'
+							onClick={() => setOpen(!open)}>
+							Details{' '}
+							<p
+								className={`${
+									open
+										? 'transition-all duration-200 ease-in-out rotate-0'
+										: '-rotate-90 transition-all duration-200 ease-in-out'
+								}`}>
+								<FaArrowDown />
+							</p>
+						</button>
+					</div>
 				</div>
-				<div className='grid grid-cols-2 items-center gap-x-12 gap-y-4 text-gray-600'>
+				<div
+					className={`${
+						open ? 'grid grid-cols-2 items-center gap-x-12 gap-y-4 text-gray-600' : 'hidden'
+					}`}>
 					<p className='flex items-center gap-x-2 text-[14px]'>
 						<GoPeople size='20px' color='gray' />
 						{res.features.seats}

ef422c047e7f8a6791e63d2c81a6c9c12af17963
tallman <vedunaik@gmail.com>
2024-04-16 17:02:30 +0530

    +pwa

--- next.config.js
+++ next.config.js
@@ -1,13 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: '**', 
-          }
-        ],
-      },
-}
+const withPWA = require('@ducanh2912/next-pwa').default({
+	dest: 'public',
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  disable: false,
+  workboxOptions: {
+    disableDevLogs: true,
+  }
+});
+
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: '**',
+			},
+		],
+	},
+};
+
+module.exports = withPWA(nextConfig);
